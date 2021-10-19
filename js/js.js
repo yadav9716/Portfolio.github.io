8cde6c059a4af60a02154df512e41276e9b98872
yadav9716 <59726975+yadav9716@users.noreply.github.com>
2021-10-19 21:42:47 +0530

    MyPortfolio
    This is my Portfolio website. I made it using pure HTML, CSS and pure JavaScript.(No frameworks is used)

---
+++ js/js.js
@@ -0,0 +1,18 @@
+burger = document.querySelector('.burger')
+right = document.querySelector('.right')
+var1 = document.getElementsByClassName(".burger")
+// var1 = document.querySelector('.column2')
+
+var1.ontoggle = function() {myFunction()}
+burger.addEventListener('click', () =>{
+    console.log("hello")
+//     // right.classList.toogle('right')
+//     var1.classList.toogle('v-class-resp')
+})
+function myFunction(){
+    console.log("hello")
+    alert("This is ")
+    
+}
+
+
